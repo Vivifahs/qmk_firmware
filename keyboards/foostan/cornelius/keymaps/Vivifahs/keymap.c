ac4571c9ad241e30d165a07f2689b06d0041dc79
vcarr <357awc@gmail.com>
2021-09-06 14:42:54 -0500

    add tap keys

--- keyboards/foostan/cornelius/keymaps/Vivifahs/keymap.c
+++ keyboards/foostan/cornelius/keymaps/Vivifahs/keymap.c
@@ -31,6 +31,32 @@
 #define GSH_TAB MT(KC_TAB, KC_V)
 #define GSH_X LALT_T(KC_X)
 
+// Tap Dance keycodes
+enum td_keycodes {
+    MNV // Tab when held, `V` when tapped. `J` when doubletapped.
+};
+
+// Define a type containing as many tapdance states as you need
+typedef enum {
+    TD_NONE,
+    TD_UNKNOWN,
+    TD_SINGLE_TAP,
+    TD_SINGLE_HOLD,
+    TD_DOUBLE_SINGLE_TAP
+} td_state_t;
+
+// Create a global instance of the tapdance state type
+static td_state_t td_state;
+
+// Declare your tapdance functions:
+
+// Function to determine the current tapdance state
+td_state_t cur_dance(qk_tap_dance_state_t *state);
+
+// `finished` and `reset` functions for each tapdance keycode
+void mnv_finished(qk_tap_dance_state_t *state, void *user_data);
+void mnv_reset(qk_tap_dance_state_t *state, void *user_data);
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = LAYOUT(
       KC_TAB, KC_Q,    KC_W,    KC_F,      KC_P,   KC_B,    KC_J,     KC_L,   KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
@@ -62,8 +88,54 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [4] = LAYOUT(
       KC_TAB, KC_ESC,  KC_Q,    KC_W,      KC_E,   KC_R,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-      KC_T,   GSH_TAB, KC_A,    KC_S,      KC_D,   KC_F,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      KC_T,   TD(MNV), KC_A,    KC_S,      KC_D,   KC_F,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
      KC_LSFT, KC_1,    KC_2,    KC_3,      KC_4,   KC_5,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
      XXXXXXX, KC_LCTL, KC_Z,    KC_Z,      GSH_X,  KC_SPC, _______,   MO(3), _______, _______, _______, _______
   ),
+};
+
+// Determine the tapdance state to return
+td_state_t cur_dance(qk_tap_dance_state_t *state) {
+    if (state->count == 1) {
+        if (state->interrupted || !state->pressed) return TD_SINGLE_TAP;
+        else return TD_SINGLE_HOLD;
+    }
+
+    if (state->count == 2) return TD_DOUBLE_SINGLE_TAP;
+    else return TD_UNKNOWN; // Any number higher than the maximum state value you return above
+}
+
+// Handle the possible states for each tapdance keycode you define:
+
+void mnv_finished(qk_tap_dance_state_t *state, void *user_data) {
+    td_state = cur_dance(state);
+    switch (td_state) {
+        case TD_SINGLE_TAP:
+            register_code16(KC_V);
+            break;
+        case TD_SINGLE_HOLD:
+            register_code16(MOD_BIT(KC_TAB)); // For a layer-tap key, use `layer_on(_MY_LAYER)` here
+            break;
+        case TD_DOUBLE_SINGLE_TAP: 
+            tap_code16(KC_LPRN);
+            register_code16(KC_J);
+    }
+}
+
+void mnv_reset(qk_tap_dance_state_t *state, void *user_data) {
+    switch (td_state) {
+        case TD_SINGLE_TAP:
+            unregister_code16(KC_V);
+            break;
+        case TD_SINGLE_HOLD:
+            unregister_code16(MOD_BIT(KC_TAB)); // For a layer-tap key, use `layer_off(_MY_LAYER)` here
+            break;
+        case TD_DOUBLE_SINGLE_TAP:
+            unregister_code16(KC_J);
+    }
+}
+
+// Define `ACTION_TAP_DANCE_FN_ADVANCED()` for each tapdance keycode, passing in `finished` and `reset` functions
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [ALT_LP] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, altlp_finished, altlp_reset)
 };
